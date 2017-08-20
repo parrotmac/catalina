dfb8d161ac34a567770c4bddb5a5c9730f72bb16
Robbie Wxyz <robbiewxyz@gmail.com>
2017-08-20 14:36:23 -0600

    setup webpack and babel

---
+++ webpack.client.js
@@ -0,0 +1,50 @@
+const resolve = require ('path').resolve
+const webpack = require ('webpack')
+
+module.exports = {
+	entry: {
+		app: [ resolve ('./client/app.js') ],
+	},
+	output: {
+		path: resolve ('./site'),
+		publicPath: '/',
+		filename: 'bundle.[name].js',
+	},
+	target: 'web',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: resolve ('.'),
+				exclude: resolve ('./node_modules'),
+				use: [ {
+					loader: 'babel-loader',
+					options: {
+						presets: [ 'react', 'env' ],
+					},
+				} ],
+			},
+			{
+				test: /\.css$/,
+				include: resolve ('.'),
+				use: [ 'style-loader', 'css-loader' ],
+			},
+		],
+	},
+	resolve: {
+		modules: [ resolve ('.'), 'node_modules' ],
+		extensions: [ '.js', '.css' ],
+	},
+	plugins: [
+		...(process.env.NODE_ENV === 'production' ? [
+			new webpack.optimize.UglifyJsPlugin (),
+			new webpack.optimize.AggressiveMergingPlugin (),
+		] : [
+			new webpack.NoEmitOnErrorsPlugin (),
+		]),
+	],
+	devtool: process.env.NODE_ENV !== 'production' ? 'cheap-module-eval-source-map' : false,
+	performance: {
+		hints: false,
+	},
+}
