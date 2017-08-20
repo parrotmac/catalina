82904f8b950eaf8feba4b615ebc965aaacb8d9e1
Robbie Wxyz <robbiewxyz@gmail.com>
2017-08-20 17:23:19 -0600

    start adding interface code

--- client/app.js
+++ client/app.js
@@ -1,2 +1,7 @@
-class App extends Component {
-}
+import 'client/style'
+import Root from 'client/Root'
+
+const root = document.createElement ('div')
+root.setAttribute ('class', 'root')
+document.body.appendChild (root)
+Inferno.render (<Root/>, root)
