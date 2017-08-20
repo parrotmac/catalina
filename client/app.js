fb4862b830f562852f62b96e668a83063af0ada5
Robbie Wxyz <robbiewxyz@gmail.com>
2017-08-20 17:37:48 -0600

    export api functions to window

--- client/app.js
+++ client/app.js
@@ -1,6 +1,10 @@
 import 'client/style'
 import Root from 'client/Root'
 
+import * as api from 'client/api'
+
+window.api = api // for debugging
+
 const root = document.createElement ('div')
 root.setAttribute ('class', 'root')
 document.body.appendChild (root)
